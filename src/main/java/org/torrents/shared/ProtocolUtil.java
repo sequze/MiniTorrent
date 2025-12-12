8ce245e80a38762f937599936696b4e3f6500d1d
Амир Абдрафиков <sequze@mail.ru>
2025-12-12 19:13:49 +0300

    feat: update ProtocolUtil
    Change json message serialization: use Jackson ObjectMapper instead of Gson

--- src/main/java/org/torrents/shared/ProtocolUtil.java
+++ src/main/java/org/torrents/shared/ProtocolUtil.java
@@ -1,27 +1,26 @@
 package org.torrents.shared;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-
+/**
+ * Общий класс для обработки сообщений между клиентом и сервером
+ */
 public class ProtocolUtil {
 
-    private static final Gson gson = new GsonBuilder().create();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static byte[] serializeMessage(Message message) {
-        String json = gson.toJson(message);
+    private static byte[] serializeMessage(Message message) throws IOException {
+        String json = objectMapper.writeValueAsString(message);
         return json.getBytes(StandardCharsets.UTF_8);
     }
 
-    private static Message deserializeMessage(byte[] jsonBytes) {
+    private static Message deserializeMessage(byte[] jsonBytes) throws IOException {
         String json = new String(jsonBytes, StandardCharsets.UTF_8);
-        return gson.fromJson(json, Message.class);
+        return objectMapper.readValue(json, Message.class);
     }
+
     public static void sendMessage(OutputStream out, Message message) throws IOException {
         byte[] jsonBytes = serializeMessage(message);
         DataOutputStream dos = new DataOutputStream(out);
@@ -32,26 +31,26 @@ public class ProtocolUtil {
 
     public static Message receiveMessage(InputStream in) throws IOException {
         DataInputStream dis = new DataInputStream(in);
+        try {
+            int length = dis.readInt();
+            if (length <= 0 || length > 1_000_000)
+                throw new IOException("Invalid message length " + length);
 
-        int length = dis.readInt();
-        if (length <= 0 || length > 1_000_000)
-            throw new IOException("Invalid message length " + length);
-
-        byte[] jsonBytes = new byte[length];
-        dis.readFully(jsonBytes);
-        return deserializeMessage(jsonBytes);
+            byte[] jsonBytes = new byte[length];
+            dis.readFully(jsonBytes);
+            return deserializeMessage(jsonBytes);
+        } catch (EOFException e) {
+            // Клиент отключился
+            return null;
+        }
     }
 
-    public static void sendChunkData(OutputStream out, String fileId, int partIndex, byte[] data) throws IOException {
-        Message header = new Message(MessageType.SEND_CHUNK, Map.of(
-                "fileId", fileId,
-                "partIndex", partIndex,
-                "length", data.length
-        ));
-
-        String json = gson.toJson(header);
+    /**
+     * Отправить сообщение с данными чанка
+     */
+    public static void sendChunkData(OutputStream out, Message message, byte[] data) throws IOException {
+        String json = objectMapper.writeValueAsString(message);
         byte[] jsonBytes = json.getBytes(StandardCharsets.UTF_8);
-
         DataOutputStream dos = new DataOutputStream(out);
         dos.writeInt(jsonBytes.length);
         dos.write(jsonBytes);
@@ -59,35 +58,15 @@ public class ProtocolUtil {
         dos.flush();
     }
 
-    public static ChunkData receiveChunkData(InputStream in) throws IOException {
-
+    public static byte[] readChunkData(InputStream in, int length) throws IOException {
         DataInputStream dis = new DataInputStream(in);
-
-        int jsonLength = dis.readInt();
-        byte[] jsonBytes = new byte[jsonLength];
-        dis.readFully(jsonBytes);
-
-        String json = new String(jsonBytes, StandardCharsets.UTF_8);
-        Map<String, Object> header = gson.fromJson(json, Map.class);
-
-        Map<String, Object> payload = (Map<String, Object>) header.get("payload");
-
-        String fileId = (String) payload.get("fileId");
-        int partIndex = ((Double) payload.get("partIndex")).intValue();
-        int length = ((Double) payload.get("length")).intValue();
-
         byte[] data = new byte[length];
-        dis.readFully(data);
-
-        return new ChunkData(fileId, partIndex, data);
+        try {
+            dis.readFully(data);
+            return data;
+        } catch (EOFException e) {
+            return null;
+        }
     }
 
-    // Hold chunk
-    @AllArgsConstructor
-    @Getter
-    public static class ChunkData {
-        private final String fileId;
-        private final int partIndex;
-        private final byte[] data;
-    }
 }
